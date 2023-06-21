95cb6a57206b672b50b8b9c2dd1fcf8c31c13a6a
Pedro Henrique Araujo <pedro.henrique@easyteam.rocks>
2023-06-20 23:55:38 -0300

    update appurl

--- src/environment/environment.prod.ts
+++ src/environment/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   apiUrl: 'https://to-do-app.azurewebsites.net',
-  appUrl: 'https://to-do-list-frontend-n8lpzxbd2-pedro-henrique-araujo.vercel.app'
+  appUrl: 'https://to-do-list-frontend-lime.vercel.app'
 };
